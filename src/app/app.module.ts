4d938b9a43f8407fc331bafd0203e82f597b8ab3
unknown <imanchesebastian@gmail.com>
2023-05-14 20:57:15 +0100

    added login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
@@ -9,8 +10,19 @@ import { FourzerofourComponent } from './components/fourzerofour/fourzerofour.co
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, LandingComponent, FourzerofourComponent, NavbarComponent],
-  imports: [CommonModule, BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    LandingComponent,
+    FourzerofourComponent,
+    NavbarComponent,
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss'],
 })
-export class LandingComponent {}
+export class LandingComponent {
+  form: any;
+
+  constructor() {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+}
