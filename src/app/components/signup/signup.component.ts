2b7f58addce4a3bfa6910cf06b50602defa5b1ab
unknown <imanchesebastian@gmail.com>
2023-05-18 12:51:03 +0100

    added form validation for signup form

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl, NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent {
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
 
+  getValue(value: NgModel) {
+    console.log(value);
+  }
 }
